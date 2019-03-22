a570646884651f822f1ab6471bd11dc11d586a7c
Glen Carpenter <glen@glencarpenter.io>
2019-03-22 08:35:28 -0600

    Updates

---
+++ precache-manifest.632d95f186bdd2c403026149ac5a87b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d70ce627cc4444038113",
+    "url": "/retrotube/static/js/runtime~main.d70ce627.js"
+  },
+  {
+    "revision": "054853565653d8eb26d8",
+    "url": "/retrotube/static/js/main.05485356.chunk.js"
+  },
+  {
+    "revision": "68df49423753f8d3573a",
+    "url": "/retrotube/static/js/2.68df4942.chunk.js"
+  },
+  {
+    "revision": "054853565653d8eb26d8",
+    "url": "/retrotube/static/css/main.fe5c106f.chunk.css"
+  },
+  {
+    "revision": "67da1b166e76804b69ff2890315243b1",
+    "url": "/retrotube/index.html"
+  }
+];
